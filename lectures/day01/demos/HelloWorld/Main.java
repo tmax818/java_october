a3ee1509f7860058d741ad2f64bf0a956bbf70ef
tmax818 <tylermaxwell661@gmail.com>
2022-09-25 19:45:40 -0700

    add args demo

--- lectures/day01/demos/HelloWorld/Main.java
+++ lectures/day01/demos/HelloWorld/Main.java
@@ -3,5 +3,8 @@ class Main{
     public static void main(String[] args) {
         String message = "Hello, Java World";
         System.out.println(message);
+        for(String arg : args){
+            System.out.println(arg);
+        }
     }
 }
