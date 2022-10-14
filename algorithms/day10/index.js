2c6188d2cbdae517abb62d88923c74e4cb2d54b0
tmax818 <tylermaxwell661@gmail.com>
2022-10-14 05:42:34 -0700

    update

---
+++ algorithms/day10/index.js
@@ -0,0 +1,20 @@
+
+class BSTNode{
+    constructor(data){
+        this.data = data;
+        this.left =null;
+        this.right =null
+    }
+}
+
+class BSTree{
+
+    constructor(){
+        this.root = null
+    }
+
+    addNode(data){
+        
+    }
+}
+
